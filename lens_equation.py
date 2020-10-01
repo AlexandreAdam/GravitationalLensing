a044576d232ef74f5ad9a4bbef67d4eb9c44bf0c
AlexandreAdam <adam.alexandre01123@gmail.com>
2020-09-30 23:20:14 -0400

    Lens equation seems to work, deflection field makes sense.

--- lens_equation.py
+++ lens_equation.py
@@ -4,107 +4,79 @@ beta: Source plane angular coordinate
 theta: Image plane coordinate
 kappa: Projected surface mass density, in units of the critical density Sigma
 """
-class Theta:
+
+
+def sis_profile(theta, x0=5, y0=5):
     """
-    Object to store theta x and y coordinates [theta_x, theta_y]
+    Singular Isothermal Sphere kappa = 1/2x, but in vector form here
     """
-    def __init__(self, theta_x, theta_y):
-        self.x = theta_x
-        self.y = theta_y
-
-    def rescale(self, value):
-        self.x *= value
-        self.y *= value
+    xi = np.stack([np.abs(theta[..., 0] - x0), np.abs(theta[..., 1] - y0)], axis=-1)
+    return  xi / 2 / (xi[..., 0]**2 + xi[..., 1]**2 + 1e-6)[..., np.newaxis]
 
 
-
-class Kappa:
-    """
-    Object to store the mass function and underlying coordinates
-    """
-    def __init__(self, profile, coordinates: Theta):
-        """
-        Profile should be a function of the form
-            Theta_x, Theta_y -> kappa
-        Where kappa is a mesh of real numbers
-        """
-        self.profile = profile
-        self.coordinates = coordinates
-        self._kappa = profile(coordinates)
-
-    @property
-    def kappa(self):
-        return self._kappa
-
-    # TODO work on this setter, it is not that simple.. Its gonna be ok for
-    # SIS profile, unless I unload the Sigma integral to profile...
-    @kappa.setter
-    def kappa(self, coordinates, profile=None):
-        if profile is None:
-            pi = self.profile
-        else:
-            pi = profile
-        print(f"Recalculating projected surface mass density using given "
-        "coordinates and profile {pi.__name__}")
-        self._kappa = pi(coordinates)
+def point_mass(theta, x0=7, y0=0):
+    out = np.zeros_like(theta)
+    x_index = np.unravel_index(np.argmin(np.abs(theta[..., 0] - x0)), theta[..., 0].shape)[1]
+    y_index = np.unravel_index(np.argmin(np.abs(theta[..., 1] - y0)), theta[..., 1].shape)[0]
+    out[y_index, x_index, :] += 1
+    return out
 
 
-def sis_profile(theta):
-    """
-    Singular Isothermal Sphere
-    """
-    return 1 / 2 / (np.sqrt(theta.x**2 + theta.y**2) + 1e-6)
-
-def coordinates(N: int) -> Theta:
+def coordinates(N: int):
     """
     Generate the image plane coordinates
     """
     x = np.arange(0, N, dtype=np.float32) - N // 2 - (N%2 - 1)/2
     xx, yy = np.meshgrid(x, x)
-    theta = Theta(xx, yy)
+    theta = np.stack([xx, yy], axis=-1)
     return theta
 
-def _alpha(theta: Theta, kappa: Kappa) -> np.ndarray:
+
+def alpha_integral(theta, kappa):
     """
     Given a meshgrid of N pixel, this takes O(N^4) operations to compute
     """
-    theta = np.stack([theta.x, theta.y], axis=-1)
-    kappa = kappa.kappa[..., np.newaxis, np.newaxis, np.newaxis]
     N = theta.shape[0] # number of pixels
     assert N <= 32, "Shape is too large, N <= 32"
     theta_prime = np.stack([theta] * N, axis=-1)
-    theta_prime = np.stack([theta_prime] * (N - 1), axis=-1) # add 2 dimensions
-    # to integrate over theta_prime, we make N(N - 1) copies of theta and shift
-    # them by 1, such that each copies has a different theta_{ij} in a given position.
+    theta_prime = np.stack([theta_prime] * N, axis=-1) # add 2 dimensions
+    kappa = np.stack([kappa] * N, axis=-1)
+    kappa = np.stack([kappa] * N, axis=-1)
     for i in range(0, N):
-        for j in range(1, N):
-            theta_prime[..., i, j-1] = np.roll(theta_prime[..., i, j-1], j, axis=1)
-        theta_prime[..., i, j-1] = np.roll(theta_prime[..., i, j-1], i, axis=0)
+        for j in range(0, N):
+            theta_prime[..., i, j] = np.roll(theta_prime[..., i, j], j, axis=1)
+            theta_prime[..., i, j] = np.roll(theta_prime[..., i, j], i, axis=0)
+            kappa[..., i, j] = np.roll(kappa[..., i, j], j, axis=1)
+            kappa[..., i, j] = np.roll(kappa[..., i, j], i, axis=0)
 
-    theta = theta[..., np.newaxis, np.newaxis] + 1e-6 # prepare for broadcast
-    norm = np.sqrt(theta**2 + theta_prime**2)
-    alpha = np.sum(kappa * (theta - theta_prime) / norm, axis=(3, 4)) # return a 2d tensor
+    theta = theta[..., np.newaxis, np.newaxis] # prepare for broadcast
+    norm = ((theta[..., 0, :, :] - theta_prime[..., 0, :, :])**2 + \
+                    (theta[..., 1, :, :] - theta_prime[..., 1, :, :])**2)[..., np.newaxis, :, :]
+    alpha = np.sum(kappa * (theta - theta_prime) / (norm + 1e-6), axis=(3, 4)) # return a 2d tensor
     return alpha
 
 
-def lens_equation(theta: Theta, alpha: np.ndarray) -> np.ndarray:
+def lens_equation(theta, alpha):
     # Returns beta
-    theta = np.stack([theta.x, theta.y], axis=-1)
     return theta - alpha
 
 
 if __name__ == "__main__":
     from visualization import lens_and_source
     import matplotlib.pyplot as plt
-    N = 12  # 32 is about the maximum the double integral can handle
+    N = 32  # 32 is about the maximum the double integral can handle
     theta = coordinates(N)
-    theta.rescale(10)
-    kappa = Kappa(sis_profile, theta)
-    alpha = _alpha(theta, kappa)
+    # kappa = sis_profile(theta)
+    kappa = point_mass(theta)
+    alpha = alpha_integral(theta, kappa)
     beta = lens_equation(theta, alpha)
-    im = plt.imshow(kappa.kappa)
+    im = plt.imshow(kappa[..., 1]**2 + kappa[..., 0]**2)
     plt.colorbar(im)
     plt.show()
-    im = plt.imshow(alpha[..., 0]**2 + alpha[..., 1]**2)
+    alpha = alpha[..., 1]**2 + alpha[..., 0]**2 # scalar function of position
+    im = plt.imshow(alpha)
+    plt.colorbar(im)
+    plt.show()
+    im = plt.imshow(beta[..., 1]**2 + beta[..., 0]**2)
     plt.colorbar(im)
     plt.show()
